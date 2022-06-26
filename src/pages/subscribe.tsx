268997f917ad41028c8436efea5e7c4a06a884fa
Samuel Formigheri <samuel.formigheri@hotmail.com>
2022-06-26 11:58:57 -0300

    Responsiveness subscriber page

--- src/pages/subscribe.tsx
+++ src/pages/subscribe.tsx
@@ -31,8 +31,8 @@ export function Subscribe(){
 
     return(
         <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-            <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
-                <div className="max-width[640px]">
+            <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto flex-col sm:flex-row p-8 lg:p-0">
+                <div className="max-w-[640px]">
                     <Logo />
                     <h1 className="mt-8 text=[2.5rem] leading-tight">
                         Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="text-blue-500">React JS</strong> 
